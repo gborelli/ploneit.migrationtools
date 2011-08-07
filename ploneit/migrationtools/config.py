a7d77d7e3af5b0dc4f531f4240e9862ddad1e94f
Giorgio Borelli <casaborelli@gmail.com>
2011-08-07 15:51:52 +0200

    changed FILE_PATH configuration

--- ploneit/migrationtools/config.py
+++ ploneit/migrationtools/config.py
@@ -1,9 +1,15 @@
 # -*- encoding: utf8 -*-
-FILE_PATH = '/Users/giorgio/sviluppo/ploneit/data/'
+import os
+try:
+    from App.config import getConfiguration
+    FILE_PATH = getConfiguration().product_config.get(
+                'ploneit_migrationtools')['export_data_directory']
+except:
+    FILE_PATH = '/tmp'
 
-CONTENTS_FILE = FILE_PATH + "export_contents.json"
-MEMBERS_FILE = FILE_PATH + "export_members.json"
-GROUPS_FILE = FILE_PATH + "export_groups.json"
+CONTENTS_FILE = os.path.sep.join((FILE_PATH, "export_contents.json"))
+MEMBERS_FILE = os.path.sep.join((FILE_PATH, "export_members.json"))
+GROUPS_FILE = os.path.sep.join((FILE_PATH, "export_groups.json"))
 
 
 FILE_FIELDNAMES = ('image', 'file')
